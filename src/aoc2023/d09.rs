fa093f5c5123730a17918c9ae1cfa5e94e3a30e6
Patrick Himmelmann <himmelmannp@gmail.com>
2023-12-10 01:05:22 +0100

    Day 9

---
+++ src/aoc2023/d09.rs
@@ -0,0 +1,23 @@
+fn extrapolate(data: &Vec<i64>) -> i64 {
+    return if data.iter().all(|&x| x == 0) {
+        0
+    } else {
+        data[data.len() - 1] + extrapolate(&data[..].windows(2).map(|x| x[1] - x[0]).collect())
+    };
+}
+
+pub fn f(input: crate::AocInput) -> crate::AocResult {
+    let mut res1 = 0;
+    let mut res2 = 0;
+    for mut x in input.lines().map(|x| x.unwrap()).map(|x| {
+        x.split_ascii_whitespace()
+            .map(|v| v.parse().unwrap())
+            .collect()
+    }) {
+        res1 += extrapolate(&x);
+        x.reverse();
+        res2 += extrapolate(&x);
+    }
+
+    (res1, res2).into()
+}
